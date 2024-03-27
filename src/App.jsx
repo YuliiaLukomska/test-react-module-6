0dee5fac5cae2ce95bd85685d00747dbe0b83265
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-03-27 21:47:59 +0200

    set planner

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,16 @@
-import Description from "./components/Description/Description";
-import { useEffect, useState } from "react";
-import Options from "./components/Options/Options";
-import Feedback from "./components/Feedback/Feedback";
-import Notification from "./components/Notification/Notification";
+import { AppBar } from "./components/AppBar/AppBar";
+import { TaskForm } from "./components/TaskForm/TaskForm";
+import { TaskList } from "./components/TaskList/TaskList";
+import { Layout } from "./components/Layout/Layout";
 
-function App() {
-  // const [reviews, setReviews] = useState({ good: 0, neutral: 0, bad: 0 });
-
-  const [reviews, setReviews] = useState(() => {
-    const savedReviews = localStorage.getItem("reviews");
-    if (savedReviews !== null) {
-      return JSON.parse(savedReviews);
-    } else {
-      return { good: 0, neutral: 0, bad: 0 };
-    }
-  });
-
-  const totalFeedback = reviews.good + reviews.neutral + reviews.bad;
-  const positiveValue = Math.round(
-    ((reviews.good + reviews.neutral) / totalFeedback) * 100
-  );
-  const updateFeedback = (feedbackType) => {
-    if (feedbackType === "reset") {
-      setReviews({ good: 0, neutral: 0, bad: 0 });
-    } else {
-      setReviews({ ...reviews, [feedbackType]: reviews[feedbackType] + 1 });
-    }
-  };
-
-  const feedbackValuesReset = () => {
-    setReviews({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-
-  useEffect(() => {
-    localStorage.setItem("reviews", JSON.stringify(reviews));
-  }, [reviews]);
-
+const App = () => {
   return (
-    <>
-      <Description />
-      <Options
-        reviewsObj={reviews}
-        updateReviews={updateFeedback}
-        totalFeedbackValue={totalFeedback}
-        feedbackReset={feedbackValuesReset}
-      />
-      {totalFeedback > 0 ? (
-        <Feedback
-          reviewsObj={reviews}
-          goodReview={reviews.good}
-          neutralReview={reviews.neutral}
-          badReview={reviews.bad}
-          totalFeedbackValue={totalFeedback}
-          positiveValuePercent={positiveValue}
-        />
-      ) : (
-        <Notification />
-      )}
-    </>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
   );
-}
+};
+
 export default App;
---
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import { getStatusFilter } from "../redux/selectors";
+import { statusFilters } from "../redux/constants";
+import { setStatusFilter } from "../redux/actions";
+import { Button } from "../Button/Button";
+
+export const StatusFilter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getStatusFilter);
+
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
+
+  return (
+    <div>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
